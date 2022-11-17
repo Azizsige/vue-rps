f68bb017d154ce8abe87693f617ee34f5dc72ec6
azizsige <kurniawan00azizsige@gmail.com>
2022-11-17 14:49:38 +0700

    test: responsive and score localstorage

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="wrapper absolute top-5 left-5">
-    <h1 class="text-3xl xl:text-2xl text-primary">Score</h1>
-    <div class="wrapper-score flex w-[8rem] justify-between mt-3">
-      <div class="score__player">
+  <div
+    class="wrapper absolute top-0 py-1 xl:py-0 xl:top-5 xl:left-5 xl:right-auto left-0 right-0 bg-white xl:bg-transparent"
+  >
+    <h1 class="text-base xl:text-2xl text-primary">Score</h1>
+    <div
+      class="wrapper-score flex w-full xl:w-[10rem] justify-center xl:justify-between mt-1 xl:mt-3"
+    >
+      <div class="score__player text-sm xl:text-xl mr-5 xl:mr-0">
         <h2>Player</h2>
         <p>{{ playerScore }}</p>
       </div>
-      <div class="score__player">
+      <div class="score__player text-sm xl:text-xl">
         <h2>Computer</h2>
         <p>{{ computerScore }}</p>
       </div>
